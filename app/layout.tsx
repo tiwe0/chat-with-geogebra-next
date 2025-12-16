c1c6af196ffb2604429bf8a092aba1142cfe65f0
ivory <contact@ivory.cafe>
2025-12-16 10:47:30 +0800

    update: local deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         {/* 添加KaTeX CSS */}
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+          href="/css/katex.min.css"
           integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV"
           crossOrigin="anonymous"
         />
